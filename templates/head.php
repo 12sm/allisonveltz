ee15e205028d824540c38c01e5f7980560ce88e4
Ian Noyes <ian@12southmusic.com>
2014-07-28 13:51:40 -0500

    font stuff?

--- templates/head.php
+++ templates/head.php
@@ -21,7 +21,8 @@ http://12southmusic.com/
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<script type="text/javascript" src="//use.typekit.net/mgy0nbs.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
