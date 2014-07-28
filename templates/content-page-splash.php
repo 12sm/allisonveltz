707a04d6854ae21e106830a2eb4f8a99f9d44199
Ian Noyes <ian@12southmusic.com>
2014-07-28 13:20:55 -0500

    splash page template

---
+++ templates/content-page-splash.php
@@ -0,0 +1,30 @@
+<div class="row av-bg">
+	<div class='col-sm-9'>
+	</div>
+	<div class='col-sm-3'>
+		<div class="sign-up">
+			<h2>Sign Up!</h2>
+			<p class='blue'>Keep up to date with the latest music, videos, & more.</p>
+			<div class="form-container">
+				<link href="https://app.e2ma.net/css/signup.med.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="https://app.e2ma.net/app2/audience/tts_signup/1763819/55ccffe82334411b90c492e32a9982ae/1738434/?v=a"></script><div id="load_check" class="signup_form_message" >This form needs Javascript to display, which your browser doesn't support. <a href="https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a"> Sign up here</a> instead </div><script type="text/javascript">signupFormObj.drawForm();</script>
+			</div>
+		</div>
+	</div>
+</div>
+<div class='row line-bg'>
+	<div class="col-sm-8 col-sm-offset-2">
+	<h1>Allison Veltz "Bright Side" Official Lyric Video</h1>
+		<div class='vid-container'>
+		<iframe width="560" height="315" src="//www.youtube.com/embed/Atu4FQ4zGVM" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class='col-sm-6 col-sm-offset-3'>
+		<h2>Full Site Coming Soon</h2>
+		<p class='blue'>In the meantime let's Tweet!</p>
+		<div class="tweet-box">
+		<textarea id="question_text" type="text">#TALKTOLIZ Type here to send me a message! @elizabethhuett</textarea><a id="talk_btn" href="https://twitter.com/intent/tweet">TALK</a>
+		</div>
+	</div>
+</div>
