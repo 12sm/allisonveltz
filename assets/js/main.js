d96ebb1e144baa80c1750f624536c411873cb903
Ian Noyes <ian@12southmusic.com>
2014-07-28 17:22:11 -0500

    new scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,17 @@ var Roots = {
   home: {
     init: function() {
       // JavaScript to be fired on the home page
+      var $talkbtn,$question,hashtags,twitter_url,update_widgets;
+      $talkbtn=$("#talk_btn");
+      $question=$("#question_text");
+      twitter_url="https://twitter.com/intent/tweet";
+      hashtags="brightside";
+      update_widgets=function(){
+        var e,t;
+        e=encodeURIComponent($question.val());
+        t=""+twitter_url+"?hashtags="+hashtags+"&text="+e;return $talkbtn.attr("href",t)};
+        $question.on("keyup",function(e){return update_widgets()});
+        $talkbtn.on("click ",function(e){e.preventDefault();update_widgets();return!0});
     }
   },
   // About us page, note the change from about-us to about_us.
