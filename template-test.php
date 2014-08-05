5af59c4fcc7082c5d37f1b2ea889087c3f8da054
Ian Noyes <ian@12southmusic.com>
2014-08-05 17:42:48 -0500

    add testing template

---
+++ template-test.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Test Template
+*/
+?>
+
+<?php get_template_part('templates/content', 'page-test'); ?>
---
+++ templates/content-page-test.php
@@ -0,0 +1,37 @@
+<div class="row av-bg">
+	<div class='col-lg-9 col-sm-7'>
+	</div>
+	<div class='col-lg-3 col-sm-5'>
+		<div class="sign-up">
+			<h2>Sign Up!</h2>
+			<p class='blue'>Keep up to date with the latest music, videos, & more.</p>
+			<div class="form-container">
+				<link href="https://app.e2ma.net/css/signup.med.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="https://app.e2ma.net/app2/audience/tts_signup/1763819/55ccffe82334411b90c492e32a9982ae/1738434/?v=a"></script><div id="load_check" class="signup_form_message" >This form needs Javascript to display, which your browser doesn't support. <a href="https://app.e2ma.net/app2/audience/signup/1763819/1738434/?v=a"> Sign up here</a> instead </div><script type="text/javascript">signupFormObj.drawForm();</script>
+			</div>
+		</div>
+	</div>
+</div>
+<div class='row line-bg'>
+	<div class="col-sm-8 col-sm-offset-2">
+	<h1>Allison Veltz "Bright Side" Official Lyric Video</h1>
+		<div class='vid-container'>
+		<iframe width="560" height="315" src="//www.youtube.com/embed/Atu4FQ4zGVM" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+</div>
+<div class="row line-bg">
+	<div class="col-sm-8 col-sm-offset-2">
+		<h1>Click to hear 4 songs from my album, Allison's Wonderland</h1>
+		<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/45902151%3Fsecret_token%3Ds-KwwhL&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+	</div>
+</div>
+<div class="row bottom-row">
+	<div class='col-sm-6 col-sm-offset-3 center-text'>
+		<h2>Full Site Coming Soon</h2>
+		<p class='blue'>In the meantime let's Tweet!</p>
+		<div class="tweet-box">
+		<textarea id="question_text" type="text">#brightside Talk to Allison! @AllisonVeltz</textarea>
+		<a id="talk_btn" target="_blank" href="https://twitter.com/intent/tweet"><i class='fa fa-twitter'></i> {Tweet Me!} </a>
+		</div>
+	</div>
+</div>
