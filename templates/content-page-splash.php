95c3ba932172dd5c47e7490856d4edbb4b25461e
Ian Noyes <ian@12southmusic.com>
2014-08-06 14:10:21 -0500

    listen to four songs

--- templates/content-page-splash.php
+++ templates/content-page-splash.php
@@ -3,7 +3,7 @@
 	</div>
 	<div class='col-lg-4 col-sm-5'>
 		<div class="sign-up">
-			<h3>Click to hear 4 songs from my album, Allison's Wonderland</h3>
+			<h3>Listen to 4 songs from my album, Allison's Wonderland</h3>
 		<iframe width="100%" height="275" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/45902151%3Fsecret_token%3Ds-KwwhL&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
 		</div>
 	</div>
